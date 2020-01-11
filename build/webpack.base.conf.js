5846fa4182b5cb3436c4744a2bc0ececf036fd5f
penov <x_pan2016@163.com>
2020-01-11 17:58:32 +0800

    stylus

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -20,7 +20,7 @@ const createLintingRule = () => ({
 })
 
 module.exports = {
-  context: path.resolve(__dirname, '../'),
+  // context: path.resolve(__dirname, '../'),
   entry: {
     app: './src/main.js'
   },
@@ -34,8 +34,10 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js',
+      // 'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      'views': resolve('src/views'),
+      'common': resolve('src/common'),
     }
   },
   module: {
